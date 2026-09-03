1005694968a36b5dc0d7467ab78442c997bc3e79
11ll11l1l1l <138889199+11ll11l1l1l@users.noreply.github.com>
2026-09-04 08:33:17 +0900

    Add BibleQuest cloud configuration scaffold

---
+++ cloud-config.js
@@ -0,0 +1,8 @@
+// BibleQuest cloud activation settings.
+// Publishable keys are designed for browser use; never place a secret/service-role key here.
+window.BQ_CLOUD_CONFIG = Object.freeze({
+  enabled: false,
+  supabaseUrl: '',
+  publishableKey: '',
+  redirectUrl: 'https://11ll11l1l1l.github.io/BibleQuest/'
+});
