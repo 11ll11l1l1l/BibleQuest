7afd429bd422b59f68c8b8b757ba0f7c20712788
11ll11l1l1l <138889199+11ll11l1l1l@users.noreply.github.com>
2026-09-04 00:23:39 +0900

    Refresh offline shell for recall deck UI

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='biblequest-v3';
-const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+const CACHE='biblequest-v4';
+const SHELL=['./','./index.html','./styles.css','./decks.css','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
