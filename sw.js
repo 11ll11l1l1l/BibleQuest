557ffa170a7a2e55c4cf7b3f5a302c289a4560c1
11ll11l1l1l <138889199+11ll11l1l1l@users.noreply.github.com>
2026-09-04 01:00:02 +0900

    Cache Transformation module offline

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='biblequest-v7';
-const SHELL=['./','./index.html','./styles.css','./decks.css','./reader.css','./sequence.css','./storyjourney.css','./app.js','./reader.js','./sequence.js','./storyjourney.js','./manifest.webmanifest'];
+const CACHE='biblequest-v8';
+const SHELL=['./','./index.html','./styles.css','./decks.css','./reader.css','./sequence.css','./storyjourney.css','./transformation.css','./app.js','./reader.js','./sequence.js','./storyjourney.js','./transformation.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
