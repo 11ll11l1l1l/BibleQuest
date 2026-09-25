707b3fd77889ecacf92256d442172da60aeb548a
11ll11l1l1l <138889199+11ll11l1l1l@users.noreply.github.com>
2026-09-25 19:25:57 +0900

    V6 Media: export Recordings runtime through media boundary

--- src/v6/media/index.ts
+++ src/v6/media/index.ts
@@ -4,3 +4,6 @@ export * from './session.ts';
 export * from './visibility-lifecycle.ts';
 export * from './youtube-iframe-adapter.ts';
 export * from './youtube-iframe-loader.ts';
+
+export * from './recordings-audio-compat.ts';
+export * from './recordings-runtime.ts';
