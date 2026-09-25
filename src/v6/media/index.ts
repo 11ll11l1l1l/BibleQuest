9a0b1a673b656b3ffc4a71b0c16a68e52a0b7945
11ll11l1l1l <138889199+11ll11l1l1l@users.noreply.github.com>
2026-09-25 11:27:13 +0900

    feat(v6): add YouTube IFrame API loader seam

--- src/v6/media/index.ts
+++ src/v6/media/index.ts
@@ -2,3 +2,4 @@ export * from './contracts.ts';
 export * from './registry.ts';
 export * from './session.ts';
 export * from './youtube-iframe-adapter.ts';
+export * from './youtube-iframe-loader.ts';
